3e3d6b2d775788a500d124d1c8db3dbe31be8c76
Victor Ching <v@victor.is>
2012-08-15 16:05:47 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chinchin
+
+Find out more about Chinchin at http://chinchinapp.com/ or visit the official blog at http://blog.chinchinapp.com/.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chinchin2::Application.config.secret_token = 'bba249fd1164426d3247fe4f6da6b63aa4008aa2b975cff14a43bb150db2d54991b5571dd4dfe7592772832772ddfc78c411b9dac7649bb69aa09aaa9a2ef7f8'
