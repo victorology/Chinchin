42ea7d04539496f98692b00486df870bbb2ff4f8
Victor Ching <v@victor.is>
2012-08-15 16:43:32 +0900

    Added code so FB canvas app does not use application.html.erb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,9 @@ class StaticPagesController < ApplicationController
   def home
   end
 
+  # This controller is for a Facebook canvas app
   def fb4pp01
+  	# This code is added so the template does not use application.html.erb
+  	render :layout => false
   end
 end
