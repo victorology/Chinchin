bdc42a84772b29440521668695feac5255b34418
Victor Ching <v@victor.is>
2012-08-18 21:59:07 +0900

    Added fb4pp01 controller to detect if uesr likes page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -16,10 +16,7 @@ class StaticPagesController < ApplicationController
   def fb4pp01
   	# This code is added so the template does not use application.html.erb
   	render :layout => false
-  end
-
-  def fbindex
-      app_secret = '035385fe9e7db60eba187ab83fb27cb6'
-      @signed_request = FBGraph::Canvas.parse_signed_request(app_secret, params[:signed_request])
+  	app_secret = '035385fe9e7db60eba187ab83fb27cb6'
+    @signed_request = FBGraph::Canvas.parse_signed_request(app_secret, params[:signed_request])
   end
 end
