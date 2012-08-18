eae14daae5b9a898f59413236d45c8f01d440b73
Victor Ching <v@victor.is>
2012-08-18 22:35:36 +0900

    Got rid of signed request code that did not work

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -16,7 +16,5 @@ class StaticPagesController < ApplicationController
   def fb4pp01
   	# This code is added so the template does not use application.html.erb
   	render :layout => false
-  	app_secret = '99daa0e54e6f549128d1f1f5b51991b3'
-    @signed_request = FBGraph::Canvas.parse_signed_request(app_secret, params[:signed_request])
   end
 end
