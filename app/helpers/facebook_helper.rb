aad7da028715b4ea9723bf88221373379a9a3c90
Victor Ching <v@victor.is>
2012-08-22 22:30:08 +0900

    Hurray! Last commit got the Facebook fan gate working!

--- app/helpers/facebook_helper.rb
+++ app/helpers/facebook_helper.rb
@@ -4,13 +4,13 @@ module FacebookHelper
 	  return fb_request['page']['liked'] if fb_request && fb_request['page']
 	end
 
-def parse_signed_request
-  if params[:signed_request].present?
-    sig, payload = params[:signed_request].split('.')
-    payload += '=' * (4 - payload.length.modulo(4))
-    data = Base64.decode64(payload.tr('-_','+/'))
-    JSON.parse( data )
-  end
+	def parse_signed_request
+	  if params[:signed_request].present?
+	    sig, payload = params[:signed_request].split('.')
+	    payload += '=' * (4 - payload.length.modulo(4))
+	    data = Base64.decode64(payload.tr('-_','+/'))
+	    JSON.parse( data )
+	  end
 	rescue Exception => e
 	  Rails.logger.warn "!!! Error parsing signed_request"
 	  Rails.logger.warn e.message
