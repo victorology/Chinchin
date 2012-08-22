58636a06be41194ef2af9e135f8eb07e240aa701
Victor Ching <v@victor.is>
2012-08-22 21:59:47 +0900

    Trying to get liked page? to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -67,10 +67,10 @@ class User < ActiveRecord::Base
 		@friends ||= facebook.friends
 	end
 
-	def user_likes_page
-    me = FbGraph::User.me(oauth_token)
+	def likes_page
+		me = FbGraph::User.me(user.oauth_token)
     page = FbGraph::Page.new(173946219392425) # or a Page retrieved using FbGraph::Page.search('fb_graph') as well
-    me.like?(page) # true if User Liked, false otherwise
+    @likes_page = me.like?(page) # true if User Liked, false otherwise
   end
 
 end
