7fd449f40a79c387b542bf7ae96ba28c241b9c80
Victor Ching <v@victor.is>
2012-08-22 22:07:47 +0900

    I give up on this for now

--- app/helpers/facebook_helper.rb
+++ app/helpers/facebook_helper.rb
@@ -1,2 +1,7 @@
 module FacebookHelper
+	def user_likes_page
+    me = FbGraph::User.me(user.oauth_token)
+    page = FbGraph::Page.new(173946219392425) # or a Page retrieved using FbGraph::Page.search('fb_graph') as well
+    @user_likes_page = me.like?(page) # true if User Liked, false otherwise
+  end
 end
