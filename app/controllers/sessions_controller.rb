de35dfb8f663d46e18aac8d05522c29b9a8b7656
Victor Ching <v@victor.is>
2012-12-05 18:38:29 +0900

    Redirecting to previous url not working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    # Changed from Railscast to redirect to old url as opposed to root_url
-    redirect_to(:back)
+    redirect_to root_url
   end
 
   def destroy
