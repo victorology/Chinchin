d7ae4630bb618cdf8111c016e26b5755034005eb
Victor Ching <v@victor.is>
2012-12-12 21:08:36 +0900

    Created base i18n functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  # Required for i18n 
+  before_filter :set_locale
+
   # This is a method to fetch the current user via OmniAuth
   private
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
     rescue ActiveRecord::RecordNotFound
   end
   helper_method :current_user
+
+  # Added for i18n
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale].present?
+  end
 
 end
