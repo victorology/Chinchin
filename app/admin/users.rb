d9db4d822ef89a6533b779f245fbff9212ebb15d
Victor Ching <v@victor.is>
2012-12-16 17:09:01 +0900

    Added ActiveAdmin

---
+++ app/admin/users.rb
@@ -0,0 +1,9 @@
+ActiveAdmin.register User do
+  index do
+  	column "Name", :name
+  	column "Gender", :gender
+  	column "Language", :locale
+  	column "Join Date", :created_at
+  	column "Update Date", :updated_at
+  end
+end
