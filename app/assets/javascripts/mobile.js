82f41bd55e494f8edd9200e95dc7a85fabe518df
Charlie Kim <nabugoon@gmail.com>
2013-03-21 16:57:34 +0900

    [ADD] Jquery-mobile for mobile app [ADD] sidepanel for menu

---
+++ app/assets/javascripts/mobile.js
@@ -0,0 +1,10 @@
+$( document ).on( "pageinit", "body", function() {
+    $( document ).on( "swiperight", "#chinchin", function( e ) {
+        // We check if there is no open panel on the page because otherwise
+        // a swipe to close the left panel would also open the right panel (and v.v.).
+        // We do this by checking the data that the framework stores on the page element (panel: open).
+        if ( $.mobile.activePage.jqmData( "panel" ) !== "open" ) {
+            $( "#menupanel" ).panel( "open" );
+        }
+    });
+});
