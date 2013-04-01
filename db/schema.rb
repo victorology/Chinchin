823e835d295d19ce3eca1b766add3e3ca82f9134
Victor Ching <v@victor.is>
2013-04-01 22:32:55 +0900

    Ran rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -14,14 +14,14 @@
 ActiveRecord::Schema.define(:version => 20130401114611) do
 
   create_table "active_admin_comments", :force => true do |t|
-    t.string    "resource_id",   :null => false
-    t.string    "resource_type", :null => false
-    t.integer   "author_id"
-    t.string    "author_type"
-    t.text      "body"
-    t.timestamp "created_at",    :null => false
-    t.timestamp "updated_at",    :null => false
-    t.string    "namespace"
+    t.string   "resource_id",   :null => false
+    t.string   "resource_type", :null => false
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.text     "body"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.string   "namespace"
   end
 
   add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
@@ -29,71 +29,71 @@ ActiveRecord::Schema.define(:version => 20130401114611) do
   add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
 
   create_table "admin_users", :force => true do |t|
-    t.string    "email",                  :default => "", :null => false
-    t.string    "encrypted_password",     :default => "", :null => false
-    t.string    "reset_password_token"
-    t.timestamp "reset_password_sent_at"
-    t.timestamp "remember_created_at"
-    t.integer   "sign_in_count",          :default => 0
-    t.timestamp "current_sign_in_at"
-    t.timestamp "last_sign_in_at"
-    t.string    "current_sign_in_ip"
-    t.string    "last_sign_in_ip"
-    t.timestamp "created_at",                             :null => false
-    t.timestamp "updated_at",                             :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
   end
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
   create_table "chinchins", :force => true do |t|
-    t.string    "uid"
-    t.string    "name"
-    t.string    "email"
-    t.string    "first_name"
-    t.string    "last_name"
-    t.string    "birthday"
-    t.string    "location"
-    t.string    "hometown"
-    t.string    "employer"
-    t.string    "position"
-    t.string    "gender"
-    t.string    "relationship_status"
-    t.string    "school"
-    t.string    "locale"
-    t.timestamp "created_at",          :null => false
-    t.timestamp "updated_at",          :null => false
-    t.integer   "user_id"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "birthday"
+    t.string   "location"
+    t.string   "hometown"
+    t.string   "employer"
+    t.string   "position"
+    t.string   "gender"
+    t.string   "relationship_status"
+    t.string   "school"
+    t.string   "locale"
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
+    t.integer  "user_id"
   end
 
   create_table "delayed_jobs", :force => true do |t|
-    t.integer   "priority",   :default => 0
-    t.integer   "attempts",   :default => 0
-    t.text      "handler"
-    t.text      "last_error"
-    t.timestamp "run_at"
-    t.timestamp "locked_at"
-    t.timestamp "failed_at"
-    t.string    "locked_by"
-    t.string    "queue"
-    t.timestamp "created_at",                :null => false
-    t.timestamp "updated_at",                :null => false
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "friendships", :force => true do |t|
-    t.integer   "chinchin_id"
-    t.integer   "user_id"
-    t.timestamp "created_at",  :null => false
-    t.timestamp "updated_at",  :null => false
+    t.integer  "chinchin_id"
+    t.integer  "user_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "likes", :force => true do |t|
-    t.integer   "chinchin_id"
-    t.integer   "user_id"
-    t.timestamp "created_at",  :null => false
-    t.timestamp "updated_at",  :null => false
+    t.integer  "chinchin_id"
+    t.integer  "user_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "profile_photos", :force => true do |t|
@@ -106,25 +106,25 @@ ActiveRecord::Schema.define(:version => 20130401114611) do
   end
 
   create_table "users", :force => true do |t|
-    t.string    "provider"
-    t.string    "uid"
-    t.string    "name"
-    t.string    "email"
-    t.string    "first_name"
-    t.string    "last_name"
-    t.string    "birthday"
-    t.string    "location"
-    t.string    "hometown"
-    t.string    "employer"
-    t.string    "position"
-    t.string    "gender"
-    t.string    "relationship_status"
-    t.string    "school"
-    t.string    "locale"
-    t.string    "oauth_token"
-    t.timestamp "oauth_expires_at"
-    t.timestamp "created_at",          :null => false
-    t.timestamp "updated_at",          :null => false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "birthday"
+    t.string   "location"
+    t.string   "hometown"
+    t.string   "employer"
+    t.string   "position"
+    t.string   "gender"
+    t.string   "relationship_status"
+    t.string   "school"
+    t.string   "locale"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
   end
 
   create_table "views", :force => true do |t|
