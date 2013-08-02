fae1c6a86ce8ce596b21c5ef4babb02f0ac4c1f1
Charlie Kim <nabugoon@gmail.com>
2013-08-02 15:21:56 +0900

    [REMOVE] seperate migration about profile_photo (too big)

--- script/step3.rb
+++ script/step3.rb
@@ -1,35 +1,29 @@
-Friendship.all.each do |model|
-	p model.id
-	tempChinchin = TempChinchin.find_by_chinchin_id(model.chinchin_id)
-	if tempChinchin.nil?
-		puts "doesn't exist #{model.id} - #{model.chinchin_id}"
-	else
+ActiveRecord::Base.transaction do
+	Friendship.all.each do |model|
+		p model.id
+		tempChinchin = TempChinchin.find_by_chinchin_id(model.chinchin_id)
 		model.chinchin_id = tempChinchin.user_id
 		model.save
 	end
+	puts "Friendship ended"
 end
-puts "Friendship ended"
 
-Like.all.each do |model|
-	p model.id
-	tempChinchin = TempChinchin.find_by_chinchin_id(model.chinchin_id)
-	model.chinchin_id = tempChinchin.user_id
-	model.save
+ActiveRecord::Base.transaction do
+	Like.all.each do |model|
+		p model.id
+		tempChinchin = TempChinchin.find_by_chinchin_id(model.chinchin_id)
+		model.chinchin_id = tempChinchin.user_id
+		model.save
+	end
+	puts "Like ended"
 end
-puts "Like ended"
 
-View.all.each do |model|
-	p model.id
-	tempChinchin = TempChinchin.find_by_chinchin_id(model.viewee_id)
-	model.viewee_id = tempChinchin.user_id
-	model.save
+ActiveRecord::Base.transaction do
+	View.all.each do |model|
+		p model.id
+		tempChinchin = TempChinchin.find_by_chinchin_id(model.viewee_id)
+		model.viewee_id = tempChinchin.user_id
+		model.save
+	end
+	puts "View ended"
 end
-puts "View ended"
-
-ProfilePhoto.all.each do |model|
-	p model.id
-	tempChinchin = TempChinchin.find_by_chinchin_id(model.chinchin_id)
-	model.chinchin_id = tempChinchin.user_id
-	model.save
-end
-puts "Profile Photos ended"
