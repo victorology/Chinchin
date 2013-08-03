31254d07550357ada24bea8d38ef3922cd4db027
Charlie Kim <nabugoon@gmail.com>
2013-08-03 10:00:32 +0900

    [FIX] view logic

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -19,7 +19,7 @@ class ViewsController < ApplicationController
     viewee = User.find(params[:viewee_id].to_i)
     viewer = current_user
     if not viewee.nil? and not View.find_by_viewer_id_and_viewee_id(viewer.id, viewee.id)
-      InteractionManager.view(:actor => current_user, :receiver => viewer)
+      InteractionManager.view(:actor => viewer, :receiver => viewee)
     end
 
     #   v = View.new
