1fb411687eef627303ea4d4d819df19454b11ecb
Victor Ching <v@victor.is>
2013-08-25 14:56:55 +0900

    Reverted subdomain changes

--- config/application.rb
+++ config/application.rb
@@ -72,9 +72,6 @@ module Chinchin2
     config.generators do |g|
       g.test_framework  :rspec, :fixture => true
       g.fixture_replacement :factory_girl, :dir=>"spec/factories"
-
-    # Added for subdomain processing
-    config.autoload_paths += %W(#{config.root}/lib)
     end
   end
 end
