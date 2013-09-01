132d78020a2752a41b91fb9dd2144cd95860ca11
Victor Ching <v@victor.is>
2013-09-01 17:03:17 +0900

    Changed wording and styling of checkbox

--- lib/subdomain.rb
+++ lib/subdomain.rb
@@ -2,5 +2,6 @@ class Subdomain
   def self.matches?(request)  
     request.subdomain.present? && request.subdomain != 'www'
     request.subdomain.present? && request.subdomain != 'skunkwrx'  
+    request.subdomain.present? && request.subdomain != 'test'
   end  
 end
