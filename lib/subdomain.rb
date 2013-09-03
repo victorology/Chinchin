1bf1f937e13814043af151610bbcf8917270f384
Victor Ching <v@victor.is>
2013-09-03 16:47:07 +0900

    Applied new My Likes design

--- lib/subdomain.rb
+++ lib/subdomain.rb
@@ -1,7 +1,7 @@
 class Subdomain  
   def self.matches?(request)  
     request.subdomain.present? && request.subdomain != 'www'
-    request.subdomain.present? && request.subdomain != 'skunkwrx'  
+    request.subdomain.present? && request.subdomain != 'skunkwrx'
     request.subdomain.present? && request.subdomain != 'test'
   end  
 end
