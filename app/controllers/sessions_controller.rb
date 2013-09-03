6aa29812debe4973ab18206a488cb2fdf82340a0
Charlie Kim <nabugoon@gmail.com>
2013-09-03 20:14:52 +0900

    hotfix Sign up event for whom is already in User table but not registered yet

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
 
     if user.status == User::UNREGISTERED
       user.update_from_omniauth(auth)
+      next_url = tutorials_url
     end
 
     unless user
